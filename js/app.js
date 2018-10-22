f71c44c5565cfaab4695cce75658b5b2428bce67
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-10-21 18:21:09 -1000

    pau4

--- js/app.js
+++ js/app.js
@@ -39,8 +39,16 @@ function zoom(){
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
+var menuItems = ['McChicken', 'Ice-Cream Cone', 'McDouble'];
+function valueMenu(){
 
+for(i=0; i<menuItems.length; i++){
+    var dollar = document.createElement('li');
+dollar.innerHTML = menuItems[i];
+menu.appendChild(dollar);
+}
 
+}
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
 
