668cda1a096f35da92aab2762fb40b4ed681cbab
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-10-22 00:26:25 -1000

    pau8

--- js/app.js
+++ js/app.js
@@ -79,7 +79,15 @@ function saySum(){
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
+random.addEventListener('click', sayRandom);
+function sayRandom(){
+var randomWords = Math.floor(Math.random()*(quotes.length));
 
+var sayRandom = document.createElement('p');
+sayRandom.innerHTML= quotes[randomWords];
+displayQuotes.appendChild(sayRandom);
+
+}
 
 
 //9. Unlock the Secret to Financial Freedom
